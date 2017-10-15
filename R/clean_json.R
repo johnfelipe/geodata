abb9848be1a17da1d4a892e0fb8fce41aab56f4e
CamilaAchury <camilaachury.4@gmail.com>
2017-10-15 13:40:29 -0500

    change jsonlite by rjson for function toJSON

--- R/clean_json.R
+++ R/clean_json.R
@@ -1,28 +1,31 @@
-# # Limpieza archivo .json --------------------------------------------------
-#
+# library(geojsonio)
+# library(readr)
+# library(dplyr)
+# library(ggplot2)
 # library(jsonlite)
 #
+# # Limpieza archivo topojson --------------------------------------------------
+#
 # json_file <- read_json('URY_adm1.json')
+#
 #
 # leng_json <- length(json_file$objects$URY_adm1$geometries)
 # for(i in 1:leng_json){
 #     json_file$objects$URY_adm1$geometries[[i]]$id <- json_file$objects$URY_adm1$geometries[[i]]$properties$HASC_1
 #     json_file$objects$URY_adm1$geometries[[i]]$properties <- json_file$objects$URY_adm1$geometries[[i]]$properties[c('NAME_1')]
 #     names(json_file$objects$URY_adm1$geometries[[i]]$properties) <- 'name'
+#     json_file
 #     }
 #
 #
-# json_file <- toJSON(json_file,auto_unbox = TRUE)
+# json_file <- rjson::toJSON(json_file)
 # dir.create('inst/geodata/ury')
 # writeLines(json_file,'inst/geodata/ury/ury.topojson')
 #
 #
 # # Creacion de centroides --------------------------------------------------
 #
-# library(geojsonio)
-# library(readr)
-# library(dplyr)
-# library(ggplot2)
+#
 #
 # tj <- topojson_read('inst/geodata/ury/ury.topojson')
 #
