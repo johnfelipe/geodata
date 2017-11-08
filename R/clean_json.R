8b154feb9d5e90424634a9fe59b91b96fa1e74bc
CamilaAchury <camilaachury.4@gmail.com>
2017-11-08 17:08:05 -0500

    Se agrego El salvador, Panama, Nicaragua y Venezuela

--- R/clean_json.R
+++ R/clean_json.R
@@ -1,33 +1,26 @@
-#' library(geojsonio)
-#' library(readr)
-#' library(dplyr)
-#' library(ggplot2)
-#' library(jsonlite)
-#' #
-#' # Limpieza archivo topojson --------------------------------------------------
-#'
-#' json_file <- read_json('GTM_adm1.json')
+#' # library(geojsonio)# library(readr)# library(dplyr)# library(ggplot2)# library(jsonlite)## Limpieza archivo topojson --------------------------------------------------
+#' json_file <- read_json('VEN_adm1.json')
 #'
 #'
-#' leng_json <- length(json_file$objects$GTM_adm1$geometries)
+#' leng_json <- length(json_file$objects$VEN_adm1$geometries)
 #' for(i in 1:leng_json){
-#'     json_file$objects$GTM_adm1$geometries[[i]]$id <- json_file$objects$GTM_adm1$geometries[[i]]$properties$HASC_1
-#'     json_file$objects$GTM_adm1$geometries[[i]]$properties <- json_file$objects$GTM_adm1$geometries[[i]]$properties[c('NAME_1')]
-#'     names(json_file$objects$GTM_adm1$geometries[[i]]$properties) <- 'name'
+#'     json_file$objects$VEN_adm1$geometries[[i]]$id <- json_file$objects$VEN_adm1$geometries[[i]]$properties$HASC_1
+#'     json_file$objects$VEN_adm1$geometries[[i]]$properties <- json_file$objects$VEN_adm1$geometries[[i]]$properties[c('NAME_1')]
+#'     names(json_file$objects$VEN_adm1$geometries[[i]]$properties) <- 'name'
 #'     json_file
 #'     }
 #'
 #'
 #' json_file <- rjson::toJSON(json_file)
-#' #dir.create('inst/geodata/GTM')
-#' writeLines(json_file,'inst/geodata/gtm/gtm-departments.topojson')
+#' dir.create('inst/geodata/ven')
+#' writeLines(json_file,'inst/geodata/ven/ven-states.topojson')
 #'
 #'
 #' # Creacion de centroides --------------------------------------------------
 #'
 #'
 #'
-#' tj <- topojson_read('inst/geodata/gtm/gtm-departments.topojson')
+#' tj <- topojson_read('inst/geodata/ven/ven-states.topojson')
 #'
 #' data_map <- fortify(tj) %>% mutate(.id = as.numeric(id)) %>% select(-id)
 #'
@@ -37,7 +30,7 @@
 #' data_info$`.id` <- as.character(rownames(data_info))
 #' data_cent <- left_join(data_cent, data_info)
 #' data_cent <- data_cent %>% select(id, name, lat, lon)
-#' write_csv(data_cent, 'inst/geodata/gtm/gtm-departments.csv')
+#' write_csv(data_cent, 'inst/geodata/ven/ven-states.csv')
 #'
 #'
 #'
@@ -45,28 +38,28 @@
 #' #
 #'
 #'
-#' json_file <- read_json('GTM_adm2.json')
+#' json_file <- read_json('VEN_adm2.json')
 #'
 #'
-#' leng_json <- length(json_file$objects$GTM_adm2$geometries)
+#' leng_json <- length(json_file$objects$VEN_adm2$geometries)
 #' for(i in 1:leng_json){
-#'   json_file$objects$GTM_adm2$geometries[[i]]$id <- json_file$objects$GTM_adm2$geometries[[i]]$properties$ID_2
-#'   json_file$objects$GTM_adm2$geometries[[i]]$properties <- json_file$objects$GTM_adm2$geometries[[i]]$properties[c('NAME_2', 'NAME_1')]
-#'   names(json_file$objects$GTM_adm2$geometries[[i]]$properties) <- c('name', 'depto')
+#'   json_file$objects$VEN_adm2$geometries[[i]]$id <- json_file$objects$VEN_adm2$geometries[[i]]$properties$ID_2
+#'   json_file$objects$VEN_adm2$geometries[[i]]$properties <- json_file$objects$VEN_adm2$geometries[[i]]$properties[c('NAME_2', 'NAME_1')]
+#'   names(json_file$objects$VEN_adm2$geometries[[i]]$properties) <- c('name', 'depto')
 #'   json_file
 #' }
 #'
 #'
 #' json_file <- rjson::toJSON(json_file)
 #'
-#' writeLines(json_file,'inst/geodata/gtm/gtm-municipalities.topojson')
+#' writeLines(json_file,'inst/geodata/ven/ven-districs.topojson')
 #'
 #'
-#' #GTM_adm2
+#' #VEN_adm2
 #'
-#' #'inst/geodata/col/col-adm2-municipalities-altnames.csv'
+#' #'inst/geodata/col/col-adm2-districs-altnames.csv'
 #'
-#' tj <- topojson_read('inst/geodata/gtm/gtm-municipalities.topojson')
+#' tj <- topojson_read('inst/geodata/ven/ven-districs.topojson')
 #'
 #' data_map <- fortify(tj) %>% mutate(.id = as.numeric(id)) %>% select(-id)
 #'
@@ -77,7 +70,7 @@
 #' data_info$`.id` <- as.character(rownames(data_info))
 #' data_cent <- left_join(data_cent, data_info)
 #' data_cent <- data_cent %>% select(id, name, lat, lon)
-#' write_csv(data_cent, 'inst/geodata/gtm/gtm-municipalities.csv')
+#' write_csv(data_cent, 'inst/geodata/ven/ven-districs.csv')
 #' #
-#'
-#'
+
+
