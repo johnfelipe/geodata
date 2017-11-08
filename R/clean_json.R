f8a181f5299cdf7087b4f1d0db614617adb02add
CamilaAchury <camilaachury.4@gmail.com>
2017-11-08 15:45:46 -0500

    added topojson of Mexico, Peru, Cuba, Guatemala

--- R/clean_json.R
+++ R/clean_json.R
@@ -1,61 +1,83 @@
-# library(geojsonio)
-# library(readr)
-# library(dplyr)
-# library(ggplot2)
-# library(jsonlite)
-#
-# # Limpieza archivo topojson --------------------------------------------------
-#
-# json_file <- read_json('URY_adm1.json')
-#
-#
-# leng_json <- length(json_file$objects$URY_adm1$geometries)
-# for(i in 1:leng_json){
-#     json_file$objects$URY_adm1$geometries[[i]]$id <- json_file$objects$URY_adm1$geometries[[i]]$properties$HASC_1
-#     json_file$objects$URY_adm1$geometries[[i]]$properties <- json_file$objects$URY_adm1$geometries[[i]]$properties[c('NAME_1')]
-#     names(json_file$objects$URY_adm1$geometries[[i]]$properties) <- 'name'
-#     json_file
-#     }
-#
-#
-# json_file <- rjson::toJSON(json_file)
-# dir.create('inst/geodata/ury')
-# writeLines(json_file,'inst/geodata/ury/ury.topojson')
-#
-#
-# # Creacion de centroides --------------------------------------------------
-#
-#
-#
-# tj <- topojson_read('inst/geodata/ury/ury.topojson')
-#
-# data_map <- fortify(tj) %>% mutate(.id = as.numeric(id)) %>% select(-id)
-#
-# data_cent <- data_map %>% group_by(.id) %>% summarise(lon= median(long), lat = median(lat))
-# data_cent$.id <- as.character(data_cent$.id)
-# data_info <-tj@data
-# data_info$`.id` <- as.character(rownames(data_info))
-# data_cent <- left_join(data_cent, data_info)
-# data_cent <- data_cent %>% select(id, name, lat, lon)
-# write_csv(data_cent, 'inst/geodata/ury/ury.csv')
-#
-#
-#
-#
-#
-
-# json_file <- read_json('COL_adm2(1).json')
-#
-#
-# leng_json <- length(json_file$objects$COL_adm2$geometries)
-# for(i in 1:leng_json){
-#   json_file$objects$COL_adm2$geometries[[i]]$id <- json_file$objects$COL_adm2$geometries[[i]]$properties$ID_2
-#   json_file$objects$COL_adm2$geometries[[i]]$properties <- json_file$objects$COL_adm2$geometries[[i]]$properties[c('NAME_2', 'NAME_1')]
-#   names(json_file$objects$COL_adm2$geometries[[i]]$properties) <- c('name', 'depto')
-#   json_file
-# }
-#
-#
-# json_file <- rjson::toJSON(json_file)
-#
-# writeLines(json_file,'data/sp/col_munps.topojson')
+#' library(geojsonio)
+#' library(readr)
+#' library(dplyr)
+#' library(ggplot2)
+#' library(jsonlite)
+#' #
+#' # Limpieza archivo topojson --------------------------------------------------
+#'
+#' json_file <- read_json('GTM_adm1.json')
+#'
+#'
+#' leng_json <- length(json_file$objects$GTM_adm1$geometries)
+#' for(i in 1:leng_json){
+#'     json_file$objects$GTM_adm1$geometries[[i]]$id <- json_file$objects$GTM_adm1$geometries[[i]]$properties$HASC_1
+#'     json_file$objects$GTM_adm1$geometries[[i]]$properties <- json_file$objects$GTM_adm1$geometries[[i]]$properties[c('NAME_1')]
+#'     names(json_file$objects$GTM_adm1$geometries[[i]]$properties) <- 'name'
+#'     json_file
+#'     }
+#'
+#'
+#' json_file <- rjson::toJSON(json_file)
+#' #dir.create('inst/geodata/GTM')
+#' writeLines(json_file,'inst/geodata/gtm/gtm-departments.topojson')
+#'
+#'
+#' # Creacion de centroides --------------------------------------------------
+#'
+#'
+#'
+#' tj <- topojson_read('inst/geodata/gtm/gtm-departments.topojson')
+#'
+#' data_map <- fortify(tj) %>% mutate(.id = as.numeric(id)) %>% select(-id)
+#'
+#' data_cent <- data_map %>% group_by(.id) %>% summarise(lon= median(long), lat = median(lat))
+#' data_cent$.id <- as.character(data_cent$.id)
+#' data_info <-tj@data
+#' data_info$`.id` <- as.character(rownames(data_info))
+#' data_cent <- left_join(data_cent, data_info)
+#' data_cent <- data_cent %>% select(id, name, lat, lon)
+#' write_csv(data_cent, 'inst/geodata/gtm/gtm-departments.csv')
+#'
+#'
+#'
+#'
+#' #
+#'
+#'
+#' json_file <- read_json('GTM_adm2.json')
+#'
+#'
+#' leng_json <- length(json_file$objects$GTM_adm2$geometries)
+#' for(i in 1:leng_json){
+#'   json_file$objects$GTM_adm2$geometries[[i]]$id <- json_file$objects$GTM_adm2$geometries[[i]]$properties$ID_2
+#'   json_file$objects$GTM_adm2$geometries[[i]]$properties <- json_file$objects$GTM_adm2$geometries[[i]]$properties[c('NAME_2', 'NAME_1')]
+#'   names(json_file$objects$GTM_adm2$geometries[[i]]$properties) <- c('name', 'depto')
+#'   json_file
+#' }
+#'
+#'
+#' json_file <- rjson::toJSON(json_file)
+#'
+#' writeLines(json_file,'inst/geodata/gtm/gtm-municipalities.topojson')
+#'
+#'
+#' #GTM_adm2
+#'
+#' #'inst/geodata/col/col-adm2-municipalities-altnames.csv'
+#'
+#' tj <- topojson_read('inst/geodata/gtm/gtm-municipalities.topojson')
+#'
+#' data_map <- fortify(tj) %>% mutate(.id = as.numeric(id)) %>% select(-id)
+#'
+#' data_cent <- data_map %>% group_by(.id) %>% summarise(lon= median(long), lat = median(lat))
+#'
+#' data_cent$.id <- as.character(data_cent$.id)
+#' data_info <-tj@data
+#' data_info$`.id` <- as.character(rownames(data_info))
+#' data_cent <- left_join(data_cent, data_info)
+#' data_cent <- data_cent %>% select(id, name, lat, lon)
+#' write_csv(data_cent, 'inst/geodata/gtm/gtm-municipalities.csv')
+#' #
+#'
+#'
