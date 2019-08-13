bb66764cb75780440952df9b1f29a173bb3339e8
CamilaAchury <camilaachury.4@gmail.com>
2019-08-12 19:29:18 -0500

    cambios topojson OJO

--- R/clean_json.R
+++ R/clean_json.R
@@ -2,17 +2,17 @@
 # library(geojsonio)# library(readr)## library(dplyr)# library(ggplot2)#
 # library(jsonlite)#
 
-#' ## Limpieza archivo topojson --------------------------------------------------#json_file <- read_json('TTO_adm1.json')###
-#' json_file <- read_json('inst/geodata/tto/TTO_adm1.json')
-#' leng_json <- length(json_file$objects$TTO_adm1$geometries)#
-#' for(i in 1:leng_json){
-#'   json_file$objects$TTO_adm1$geometries[[i]]$id <- json_file$objects$TTO_adm1$geometries[[i]]$properties$ID_1
-#'   json_file$objects$TTO_adm1$geometries[[i]]$properties <- json_file$objects$TTO_adm1$geometries[[i]]$properties[c('NAME_1')]#
-#'   names(json_file$objects$TTO_adm1$geometries[[i]]$properties) <- 'name'#
-#'   json_file   }###
-#' json_file <- rjson::toJSON(json_file)
-#' # dir.create('inst/geodata/TTO')#
-#' writeLines(json_file,'inst/geodata/tto/tto-regions.topojson')###
+#' ## Limpieza archivo topojson --------------------------------------------------#json_file <- read_json('COL_adm1.json')###
+# json_file <- read_json('colombia.topojson')
+# leng_json <- length(json_file$objects$COL_adm1$geometries)#
+# for(i in 1:leng_json){
+#   json_file$objects$COL_adm1$geometries[[i]]$id <- json_file$objects$COL_adm1$geometries[[i]]$properties$id
+#   json_file$objects$COL_adm1$geometries[[i]]$properties <- json_file$objects$COL_adm1$geometries[[i]]$properties[c('NAME_1')]#
+#   names(json_file$objects$COL_adm1$geometries[[i]]$properties) <- 'name'#
+#   json_file   }###
+# json_file <- rjson::toJSON(json_file)
+# # dir.create('inst/geodata/TTO')#
+# writeLines(json_file,'colombia_aaa.topojson')###
 #' # # Creacion de centroides --------------------------------------------------
 #' # ####
 #' # tj <- topojson_read('inst/geodata/TTO/TTO-districs.topojson')##
@@ -72,4 +72,36 @@
 # install()
 # library(geodata)
 
+# mapName <- 'col_departments'
+# tmp <-  topojson_read(geodataTopojsonPath(mapName))
+#
+# #tmp <- topojson_read('col.topojson')
+# x <- geojson_list(tmp)
+# # View(tmp)
+# #
+# # # world regions
+# wr <- read_csv('inst/geodata/col/col-adm1-regions.csv')
+#
+# # asia
+# asia <- wr %>% filter(region == 'Andina')
+# asia$id <- ifelse(asia$name == 'ANTIOQUIA',gsub('5', '05', asia$id), asia$id)
+# cen <- read_csv('inst/geodata/col/col-adm1-departments.csv')
+# cen <- cen %>% filter(id %in% asia$id)
+# # write_csv(cen, 'myfule.csv')
+# tmp2 <- filter(tmp, id %in% asia$id)
+#
+# topojson_write(tmp2, "myfile.topojson")
+# tmp <-  topojson_read("myfile.topojson")
+# topoData <- readLines("myfile.topojson") %>% paste(collapse = "\n")
+# lf <- leaflet() %>%
+#   leaflet(options = leafletOptions(zoomControl = TRUE))
+# b_box <- geojson::bbox_get(topoData)
+# #
+# lf %>%
+#   addTopoJSON(topoData,
+#               weight = 1,
+#               color = '#000000',
+#               fill = FALSE)%>%
+#    setView(lng = mean(c(b_box[1],b_box[3])),lat = mean(c(b_box[2], b_box[4])), zoom = 2)
 
+#
