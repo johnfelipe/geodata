a9694a3086128d1449ebd93d3fd476210d26a04f
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-03-22 12:42:19 -0500

    need to add tests

--- tests/testthat/test-geonames.R
+++ tests/testthat/test-geonames.R
@@ -1,17 +1,17 @@
 test_that("multiplication works", {
 
-  some_nms <- c("nombre","ciudad","edad")
-  col <- which_geoname_column(some_nms)
-
-
-  d <- dplyr::tribble(
-    ~ciudad,~value,
-    "Bogotá",1,
-    "Pasto",2,
-    "Buga",3
-  )
-
-  geocode(d, mapName = "col_municipalities")
+  # some_nms <- c("nombre","ciudad","edad")
+  # col <- which_geoname_column(some_nms)
+  #
+  #
+  # d <- dplyr::tribble(
+  #   ~ciudad,~value,
+  #   "Bogotá",1,
+  #   "Pasto",2,
+  #   "Buga",3
+  # )
+  #
+  # geocode(d, mapName = "col_municipalities")
 
 
 
