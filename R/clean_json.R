bb6ea5933a2af31b4cc2adf3b6ab0a2b9352efa4
CamilaAchury <camilaachury.4@gmail.com>
2020-12-06 22:37:59 -0300

    madrid-barrios

--- R/clean_json.R
+++ R/clean_json.R
@@ -1,32 +1,32 @@
-library(geojsonio)
-library(readr)
-library(dplyr)
-library(ggplot2)
-library(jsonlite)
-
-
-
-# arreglo topojson --------------------------------------------------------
-
-#  json_file <- read_json('DISTRITOS.json')
+# library(geojsonio)
+# library(readr)
+# library(dplyr)
+# library(ggplot2)
+# library(jsonlite)
 #
-# leng_json <- length(json_file$objects$DISTRITOS$geometries)
+#
+#
+# # arreglo topojson --------------------------------------------------------
+#
+#  json_file <- read_json('BARRIOS.json')
+#
+# leng_json <- length(json_file$objects$BARRIOS$geometries)
 # for(i in 1:leng_json){
-#   json_file$objects$DISTRITOS$geometries[[i]]$id <- json_file$objects$DISTRITOS$geometries[[i]]$properties$CODDISTRIT
-#   json_file$objects$DISTRITOS$geometries[[i]]$properties <- json_file$objects$DISTRITOS$geometries[[i]]$properties[c('NOMBRE')]
-#   names(json_file$objects$DISTRITOS$geometries[[i]]$properties) <- c('name')
+#   json_file$objects$BARRIOS$geometries[[i]]$id <- json_file$objects$BARRIOS$geometries[[i]]$properties$CODBARRIO
+#   json_file$objects$BARRIOS$geometries[[i]]$properties <- json_file$objects$BARRIOS$geometries[[i]]$properties[c('NOMBRE', 'NOMDIS')]
+#   names(json_file$objects$BARRIOS$geometries[[i]]$properties) <- c('name', 'distrito')
 #   json_file
 # }
 #
 # json_file <- rjson::toJSON(json_file)
 #
-# writeLines(json_file,'inst/geodata/esp/madrid-districts.topojson')
+# writeLines(json_file,'inst/geodata/esp/madrid-barrios.topojson')
 #
 # library(tidyverse)
 # library(geojsonio)
 #
-# tj <- rgdal::readOGR("inst/geodata/esp/madrid-districts.topojson")
-# nms <- as.data.frame(topojson_read("inst/geodata/esp/madrid-districts.topojson"))
+# tj <- rgdal::readOGR("inst/geodata/esp/madrid-barrios.topojson")
+# nms <- as.data.frame(topojson_read("inst/geodata/esp/madrid-barrios.topojson"))
 # nms <- nms %>%
 #   select(-geometry) %>%
 #   dplyr::mutate(.id = 0:(nrow(.)-1))
@@ -38,8 +38,8 @@ library(jsonlite)
 #
 # info_cent <- data_map %>% group_by( .id) %>% summarise(lat = median(lat), lon = median(long))
 # data_centroide <- nms %>% left_join(info_cent)
-# data_centroide <- data_centroide[,c("id", "name",  "lat", "lon")]
-# write_csv(data_centroide, "inst/geodata/esp/madrid-districts.csv")
+# data_centroide <- data_centroide[,c("id", "name", "distrito",  "lat", "lon")]
+# write_csv(data_centroide, "inst/geodata/esp/madrid-barrios.csv")
 
 
 
@@ -62,7 +62,7 @@ library(jsonlite)
 # shapefile ---------------------------------------------------------------
 
 # library(sf)
-# s.sf <- read_sf(dsn = "DISTRITOS/DISTRITOS.shp", layer = "DISTRITOS")
+# s.sf <- read_sf(dsn = "BARRIOS/BARRIOS.shp", layer = "BARRIOS")
 #
 # st_crs(s.sf)
 # # shapefile  --------------------------------------------------------------
@@ -71,7 +71,7 @@ library(jsonlite)
 #
 # s.sp <- as(s.sf.gcs, "Spatial")
 #
-# raster::shapefile(s.sp, "DISTRITOS/DISTRITOS.shp")
+# raster::shapefile(s.sp, "BARRIOS.shp")
 # library(leaflet)
 # leaflet(s.sf.gcs) %>%
 #   addPolygons() %>%
